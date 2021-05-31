b712fb50b432b505a324d68aadb20164ebfed816
Adamis <adamisstarling@gmail.com>
2021-05-31 14:06:19 -0300

    Update PersonalExceptionHandler.java

--- src/main/java/br/com/codiub/heimdall/exceptionhandler/PersonalExceptionHandler.java
+++ src/main/java/br/com/codiub/heimdall/exceptionhandler/PersonalExceptionHandler.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.lang3.exception.ExceptionUtils;
+//import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
@@ -19,7 +19,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+//import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
